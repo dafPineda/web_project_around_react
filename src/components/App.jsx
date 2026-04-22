4128e03fc308b034a0b8c71ba0dbfb0107051b16
Dafne <lire.alarcon.06@gmail.com>
2026-04-22 09:47:42 -0600

    Feax: Enlaza usuario con base de datos

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,30 @@
 import Header from "./Header/Header"
 import Main from "./Main/Main"
 import Footer from "./Footer/Footer"
+import { useState, useEffect } from "react"
+import CurrentUserContext from "../contexts/CurrentUserContext"
+import Api from "../utils/api"
+
+const api = new Api("https://around-api.es.tripleten-services.com/v1", {Authorization:"39e7e87b-63d8-4747-bf9f-2089ed281080", "Content-Type": "application/json"})
 function App() {
+  const [currentUser, setCurrentUser] = useState({})
+  const [cards, setCards] = useState([])
+  useEffect(()=>{
+    api.getAppInfo()
+    .then(([userData, cardsData])=>{
+      setCurrentUser(userData)
+      setCards(cardsData)
+    })
+  },[])
   return (
+    <CurrentUserContext.Provider value={currentUser}>
     <div className="page">
       <Header/>
-      <Main/>
+      <Main cards={cards} setCards={setCards}/>
       <Footer/>  
     </div>
+
+    </CurrentUserContext.Provider>
   )
 }
 
